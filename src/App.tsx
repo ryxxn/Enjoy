5cb7a698b2ec20a346e7511152ad488b7b5cec83
박도륜 <88328436+ryxxn@users.noreply.github.com>
2023-10-30 01:19:39 +0900

    [Feat] 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import './styles.css';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { Login } from './pages/Login';
+import { Profile } from './pages/Profile';
+import { Stamps } from './pages/Stamps';
+import { Notice } from './pages/Notice';
+import { Media } from './pages/Media';
+import "./colors.css"
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './firebase';
 
 function App() {
+
+  const [loading, setLoading] = useState<boolean>(true)
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(true);
+  
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => { // user 판명을 듣고 
+      if(user) { // 있으면
+        setIsLoggedIn(true); // 로그인 됨
+      } else {
+        setIsLoggedIn(false); // 로그인 안됨
+      }
+      setLoading(false);
+    });
+  }, [])
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <>
+        {isLoggedIn ?
+          <Routes>
+            <Route path='/profile' element={<Profile />} />
+            <Route path='/stamps' element={<Stamps />} />
+            <Route path='/notice' element={<Notice />} />
+            {/* <Route path='/notice/:id' element={<Notice />} /> */}
+            <Route path='/media' element={<Media />} />
+            <Route path='/*' element={<Navigate to="profile" />} />
+          </Routes>
+          :
+          <Routes>
+            <Route path='/login' element={<Login />} />
+            <Route path='/*' element={<Navigate to="login" />} />
+          </Routes>
+        }
+      </>
+    </BrowserRouter >
   );
 }
 
