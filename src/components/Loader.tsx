5d122278b7f031302b9f7201cfd47bf259c69e20
박도륜 <88328436+ryxxn@users.noreply.github.com>
2023-10-30 01:24:18 +0900

    [Add] 로딩 컴포넌트 구현

---
+++ src/components/Loader.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Loading = styled.span`
+    .loader {
+        width: 48px;
+        height: 48px;
+        border-radius: 50%;
+        display: inline-block;
+        position: relative;
+        background: linear-gradient(0deg, rgba(255, 61, 0, 0.2) 33%, var(--main-color) 100%);
+        box-sizing: border-box;
+        animation: rotation 1s linear infinite;
+    }
+    .loader::after {
+        content: '';  
+        box-sizing: border-box;
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+        width: 44px;
+        height: 44px;
+        border-radius: 50%;
+        background: transparent;
+    }
+    @keyframes rotation {
+        0% { transform: rotate(0deg) }
+        100% { transform: rotate(360deg)}
+    } 
+`
+
+export const Loader = () => {
+    return (
+        <Loading></Loading>
+    )
+}
