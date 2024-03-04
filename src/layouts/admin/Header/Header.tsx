a9e9bb71ae3e72327f2b39b86039bfe3775e5093
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-03-04 21:44:08 +0900

    feat: responsive mobile nav

--- src/layouts/admin/Header/Header.tsx
+++ src/layouts/admin/Header/Header.tsx
@@ -1,22 +1,46 @@
 import React from 'react';
 import Avatar from 'src/components/avatar';
 import { useUserStore } from 'src/store/userStore';
+import MobileNav from '../Nav/MobileNav';
+import { MdMenu } from 'react-icons/md';
+import useMediaQuery from 'src/hooks/useMediaQuery';
+import { useConfirmStore } from 'src/store/useConfirmStore';
+import { signOut } from 'src/services/login.services';
 
 const Header = () => {
   const { userData } = useUserStore();
+  const [navOpen, setNavOpen] = React.useState(false);
 
-  const avatarSrc =
-    userData?.profileImage ||
-    `${process.env.PUBLIC_URL}/assets/blank-profile.png`;
+  const { isMobile } = useMediaQuery();
+
+  const { handleConfirm, onClose } = useConfirmStore();
+
+  const avatarSrc = userData?.profileImage;
 
   return (
     <header className='adminHeaderLayout'>
-      <span>ENTER 관리자 페이지에 오신 것을 환영합니다.</span>
+      {isMobile ? (
+        <>
+          <MobileNav open={navOpen} onClose={() => setNavOpen(false)} />
+          <MdMenu onClick={() => setNavOpen(!navOpen)} size={24} />
+        </>
+      ) : (
+        <span>ENTER 관리자 페이지에 오신 것을 환영합니다.</span>
+      )}
       <div>
         <Avatar
           src={avatarSrc}
           size={32}
-          onClick={() => alert('Avatar click!')}
+          onClick={() =>
+            handleConfirm({
+              text: '로그아웃 하시겠습니까?',
+              open: true,
+              onAction: () => {
+                signOut();
+                onClose();
+              },
+            })
+          }
         />
       </div>
     </header>
---
+++ src/layouts/admin/Nav/MobileNav.tsx
@@ -0,0 +1,90 @@
+import React, { useRef } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import Avatar from 'src/components/avatar';
+import Card from 'src/components/card';
+import Divider from 'src/components/divider';
+import { useUserStore } from 'src/store/userStore';
+import { MdOutlineDashboard } from 'react-icons/md';
+import { FaRegUser } from 'react-icons/fa';
+import { PiStamp } from 'react-icons/pi';
+import { MdNotificationsNone } from 'react-icons/md';
+import { TfiLayoutMediaCenterAlt } from 'react-icons/tfi';
+import { ADMIN_PATH } from 'src/routes/path';
+
+interface Props {
+  open: boolean;
+  onClose: () => void;
+}
+
+const MobileNav = ({ open, onClose }: Props) => {
+  const { userData } = useUserStore();
+
+  const backgroundRef = useRef<HTMLDivElement>(null);
+
+  const avatarSrc = userData?.profileImage;
+
+  const { pathname } = useLocation();
+
+  const isActived = (path: string) => {
+    const formattedPathname = pathname.replace(/\/$/, '');
+    const formattedPath = path.replace(/\/$/, '');
+
+    return formattedPathname.includes(formattedPath);
+  };
+
+  const handleClickBackground = (e: any) => {
+    if (e.target === backgroundRef.current) {
+      onClose();
+    }
+  };
+
+  return (
+    <div
+      className={`adminMobileNavContainer ${open ? 'open' : ''}`}
+      ref={backgroundRef}
+      onClick={handleClickBackground}
+    >
+      <nav className='adminMobileNav'>
+        <Card className='avatar'>
+          <Avatar src={avatarSrc} size={32} />
+          {userData?.userName}
+        </Card>
+        <Divider />
+        <ul className='list'>
+          <li className={isActived(ADMIN_PATH.MAIN) ? 'actived' : ''}>
+            <Link to={ADMIN_PATH.MAIN}>
+              <MdOutlineDashboard size={18} />
+              Dashboard
+            </Link>
+          </li>
+          <li className={isActived(ADMIN_PATH.USERS) ? 'actived' : ''}>
+            <Link to={ADMIN_PATH.USERS}>
+              <FaRegUser size={18} />
+              사용자 관리
+            </Link>
+          </li>
+          <li className={isActived(ADMIN_PATH.STAMPS) ? 'actived' : ''}>
+            <Link to={ADMIN_PATH.STAMPS}>
+              <PiStamp size={18} />
+              스탬프 관리
+            </Link>
+          </li>
+          <li className={isActived(ADMIN_PATH.NOTICE) ? 'actived' : ''}>
+            <Link to={ADMIN_PATH.NOTICE}>
+              <MdNotificationsNone size={18} />
+              공지사항 관리
+            </Link>
+          </li>
+          <li className={isActived(ADMIN_PATH.MEDIA) ? 'actived' : ''}>
+            <Link to={ADMIN_PATH.MEDIA}>
+              <TfiLayoutMediaCenterAlt size={18} />
+              미디어 관리
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default MobileNav;
